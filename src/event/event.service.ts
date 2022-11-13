88ccbf4d5bb7a613d47161b4b510993a2d3b0ceb
BMidhun <midhunbabygeorge@gmail.com>
2022-11-13 13:56:24 +0530

    create event services

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,7 +1,44 @@
-import { Injectable } from "@nestjs/common";
-
+import { BadRequestException, Injectable } from "@nestjs/common";
+import {Repository} from "typeorm"
+import {InjectRepository} from "@nestjs/typeorm"
+import { EventEntity } from "./entity";
+import { IEvent } from "./interface";
+import { CreateEventDTO, UpdateEventDTO } from "./dto";
 @Injectable()
-
 export class EventService{
-    
+
+    constructor(
+        @InjectRepository(EventEntity)
+        private eventRepository: Repository<EventEntity>
+    ) {
+
+    }
+
+    async getAllEvents():Promise<IEvent[]> {
+        const events = await this.eventRepository.find();
+        return events;
+    }
+
+    async getEvent(id:number) {
+        const event = await this.eventRepository.findOneBy({id})
+        if(!event)
+            throw new BadRequestException("Invalid id")
+        return event;
+    }
+
+    async createEvent(payload:CreateEventDTO) {
+        return await this.eventRepository.save({...payload});
+    }
+
+    async updateEvent(id:number, payload:UpdateEventDTO) {
+        const event = await this.getEvent(id);
+        return await this.eventRepository.save({...event,...payload});
+    }
+
+    async deleteEvent(id:number) {
+        const event = await this.getEvent(id);
+        await this.eventRepository.delete(event);
+        return event;
+    }
+
 }
