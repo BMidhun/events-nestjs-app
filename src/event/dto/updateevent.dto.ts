b45a5ec0f416aaf73bde30629bf9ea3aec6033ff
BMidhun <midhunbabygeorge@gmail.com>
2022-11-14 14:56:36 +0530

    code refactor on dto

--- src/event/dto/updateevent.dto.ts
+++ src/event/dto/updateevent.dto.ts
@@ -1,22 +1,30 @@
-import {Type} from "class-transformer";
-import {IsOptional, IsString,IsDate} from "class-validator";
+// import {Type} from "class-transformer";
+// import {IsOptional, IsString,IsDate} from "class-validator";
+import {PartialType} from "@nestjs/mapped-types"
+import { CreateEventDTO } from "./createevent.dto";
 
-export class UpdateEventDTO {
+export class UpdateEventDTO extends PartialType(CreateEventDTO) {
 
-    @IsOptional()
-    @IsString()
-    name:string;
-
-    @IsOptional()
-    @IsString()
-    description:string;
-
-    @IsOptional()
-    @Type(() => Date)
-    @IsDate()
-    when: Date;
-
-    @IsOptional()
-    @IsString()
-    address:string
 }
+
+//OR 
+
+// export class UpdateEventDTO {
+
+//     @IsOptional()
+//     @IsString()
+//     name:string;
+
+//     @IsOptional()
+//     @IsString()
+//     description:string;
+
+//     @IsOptional()
+//     @Type(() => Date)
+//     @IsDateString()
+//     when: Date;
+
+//     @IsOptional()
+//     @IsString()
+//     address:string
+// }
